60ab4869868c61269c8b6c369118833ae37bff88
white_anemone <Melissa-969@yandex.ru>
2023-05-01 23:03:20 +0300

    feat: add language change

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const container = document.createElement('section');
 const title = document.createElement('h1');
 const input = document.createElement('textarea');
 const keyboardWrapper = document.createElement('div');
+const OS = document.createElement('p');
+const languageChange = document.createElement('p');
 
 document.body.append(main);
 
@@ -15,10 +17,18 @@ title.textContent = 'RSS Виртуальная клавиатура';
 
 container.append(input);
 input.className = 'input';
-input.setAttribute('rows', '15');
+input.setAttribute('rows', '10');
 
 container.append(keyboardWrapper);
 keyboardWrapper.className = 'keyboard-wrapper';
+
+container.append(OS);
+OS.className = 'OS';
+OS.textContent = 'Клавиатура создана в операционной системе Windows';
+
+container.append(languageChange);
+languageChange.className = 'language';
+languageChange.textContent = 'Для переключения языка комбинация: левыe ctrl + alt';
 
 // Создание рядов клавиатуры
 for (let i = 0; i < 5; i++) {
@@ -67,22 +77,39 @@ let arrayKeys = Array.from(document.querySelectorAll('.key-button'));
 
 // Назначение классов спецклавишам
 arrayKeys[13].classList.add('key-button_backspace');
+arrayKeys[13].innerHTML = 'Backspace';
 arrayKeys[14].classList.add('key-button_tab');
+arrayKeys[14].innerHTML = 'Tab';
 arrayKeys[28].classList.add('key-button_del');
+arrayKeys[28].innerHTML = 'Del';
 arrayKeys[29].classList.add('key-button_capslock');
+arrayKeys[29].innerHTML = 'CapsLock';
 arrayKeys[41].classList.add('key-button_enter');
+arrayKeys[41].innerHTML = 'Enter';
 arrayKeys[42].classList.add('key-button_left-shift');
+arrayKeys[42].innerHTML = 'Shift';
 arrayKeys[53].classList.add('key-button_up-arrow');
+arrayKeys[53].innerHTML = '>';
 arrayKeys[54].classList.add('key-button_right-shift');
+arrayKeys[54].innerHTML = 'Shift';
 arrayKeys[55].classList.add('key-button_left-ctrl');
+arrayKeys[55].innerHTML = 'Ctrl';
 arrayKeys[56].classList.add('key-button_left-alt');
+arrayKeys[56].innerHTML = 'Alt';
 arrayKeys[57].classList.add('key-button_win');
+arrayKeys[57].innerHTML = 'Win';
 arrayKeys[58].classList.add('key-button_space');
+arrayKeys[58].innerHTML = 'Space';
 arrayKeys[59].classList.add('key-button_right-alt');
+arrayKeys[59].innerHTML = 'Alt';
 arrayKeys[60].classList.add('key-button_left-arrow');
+arrayKeys[60].innerHTML = '<';
 arrayKeys[61].classList.add('key-button_down-arrow');
+arrayKeys[61].innerHTML = '>';
 arrayKeys[62].classList.add('key-button_right-arrow');
+arrayKeys[62].innerHTML = '>';
 arrayKeys[63].classList.add('key-button_right-ctrl');
+arrayKeys[63].innerHTML = 'Ctrl';
 
 // Английская клавиатура
 const engKeyboard = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
@@ -101,11 +128,40 @@ const rusKeyboard = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-'
 // Переменная, отслеживающая язык клавиатуры
 let language = 'English';
 
-// Функция создания клавиатуры
+// Функция создания клавиатуры в зависимости от языка
 const createKeys = (value) => {
-  for (let i = 0; i < value.length; i++) {
+  for (let i = 0; i < 13; i++) {
+    arrayKeys[i].innerHTML = value[i];
+  }
+  for (let i = 15; i < 28; i++) {
+    arrayKeys[i].innerHTML = value[i];
+  }
+  for (let i = 30; i < 41; i++) {
+    arrayKeys[i].innerHTML = value[i];
+  }
+  for (let i = 43; i < 53; i++) {
     arrayKeys[i].innerHTML = value[i];
   }
 }
 
 createKeys(engKeyboard);
+
+// Переключение языка
+document.addEventListener('keydown', function (event) {
+  if (event.altKey && event.ctrlKey) {
+    if (language == 'English') {
+      for (let i in keyboardRowArray) {
+        i.innerHTML = '';
+      }
+      // keyboardWrapper.innerHTML = '';
+      createKeys(rusKeyboard);
+      language = 'Russian';
+    } else {
+      for (let i in keyboardRowArray) {
+        i.innerHTML = '';
+      }
+      createKeys(engKeyboard);
+      language = 'English';
+    }
+  }
+});
